80534787c11959dd642232a0a22afc6d00b0468f
Andy Guajardo <andyguajardo@mac-mini.lan>
2023-09-13 21:31:13 -0700

    Age prompt on .js, new line on index.

--- logic.js
+++ logic.js
@@ -1,2 +1,4 @@
 const input1 = prompt("Please enter your name","Name here...");
-alert(`Hello there ${input1}`);
+alert(`Hello there ${input1}, it's nice to meet you! `);
+const input2 = prompt( "How old are you?");
+alert(`${input2} years old? Wow!`);
